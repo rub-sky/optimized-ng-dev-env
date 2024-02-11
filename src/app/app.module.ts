473359fdfbdf91a65a758c4c87ffeee938681556
Ruben Piatnitsky <r.piatnitsky@gmail.com>
2024-02-11 10:28:44 -0800

    Add some new components and some routing

---
+++ src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    CommonModule,
+    AppRoutingModule
+  ],
+  providers: []
+})
+export class AppModule { }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { ProductsComponent } from './products/products.component';
+import { ProductComponent } from './product/product.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'products/:prodId',
+    component: ProductComponent
+  },
+  {
+    path: 'products',
+    component: ProductsComponent
+  }
+];
