31ea278ed4bcf93f568942f479d978646c793a09
Ruben Piatnitsky <r.piatnitsky@gmail.com>
2024-02-11 10:30:34 -0800

    Clear out app component template and add router module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
 
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     CommonModule,
+    RouterModule,
     AppRoutingModule
   ],
   providers: []
